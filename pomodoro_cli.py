800b3fb7528ed723d9841c293115d7bab7461ef2
Daniel <dborrego909@gmail.com>
2024-06-09 19:41:44 -0700

    Starting the timer

--- pomodoro_cli.py
+++ pomodoro_cli.py
@@ -1,1 +1,18 @@
-print("Hello, world!")
+# Create pomodoro timer
+
+#ingrained 25 min study timer then 5 min break timer
+
+import time
+
+
+start = input("Type START or STOP to start or stop the timer")
+
+
+while True:
+    if start == "start" or "stop":
+        print("working")
+    else:
+        print("incorrect input")
+        return False
+#timer():
+
